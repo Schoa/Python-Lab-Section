6a58df1a0dc5e47fc8843651358d74cfbc2df64c
Schoa <phungminhkhoa2006@gmail.com>
2024-11-28 17:18:31 +0700

    Lab 10

---
+++ Lab 10/Lab10_P5.py
@@ -0,0 +1,26 @@
+def permute(nums):
+    """Generate all permutations of the given list of numbers."""
+    def backtrack(start=0):
+        # Base case: if we've reached the end of the array
+        if start == len(nums):
+            result.append(nums[:])  # Append a copy of the current permutation
+            return
+        
+        for i in range(start, len(nums)):
+            # Swap the current element with the start element
+            nums[start], nums[i] = nums[i], nums[start]
+            backtrack(start + 1)  # Recurse on the next index
+            # Backtrack: swap back to restore the original array
+            nums[start], nums[i] = nums[i], nums[start]
+
+    result = []
+    backtrack()  # Start generating permutations
+    return result
+
+# Input
+nums = list(map(int, input().split()))
+
+# Output
+permutations = permute(nums)
+for perm in permutations:
+        print(" ".join(map(str, perm)))
