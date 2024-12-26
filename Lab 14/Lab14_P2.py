3f7aa6e6e4c99b66007bac3eaf767306680bca6c
Schoa <phungminhkhoa2006@gmail.com>
2024-12-26 16:52:49 +0700

    Done

--- Lab 14/Lab14_P2.py
+++ Lab 14/Lab14_P2.py
@@ -3,26 +3,36 @@ import ttkbootstrap as ttk
 
 # window 
 window = ttk.Window(themename = 'darkly')
-# Your code is here
-
-# title 
-# Your code is here
-
-# input field 
-# Your code is here
-# The entry name should be entry_int
+window.title("Length converter")
+window.geometry("300x150")
 
 def convert():
-    mile_input = entry_int.get()
+    mile_input = int(entry_widget.get())
     km_output = mile_input * 1.61
     output_string.set(km_output)
+
+# Label
+label = tk.Label(
+    window,
+    text = "Miles to Kilometer",
+    font = ("Calibri", 24, "bold")
+)
+label.grid(column = 0, row = 0)
+
+# Entry widget
+entry_widget = tk.Entry(window)
+entry_widget.grid(column = 0, row = 1)
+
+# Button
+button = tk.Button(master = window, text="Convert", command=convert)
+button.grid(row = 2, column = 0)
 
 # output
 output_string = tk.StringVar()
 output_label = ttk.Label(
     master = window, 
     text = 'Output', 
-    font = 'Calibri 24', 
+    font = 'Calibri 24',
     textvariable = output_string)
 output_label.grid(column = 0, row = 3)
 
--- Lab 14/Lab14_P3.py
+++ Lab 14/Lab14_P3.py
@@ -17,18 +17,20 @@ def calculate():
         clear_display()
         display.insert(0, "Error")
 
-# Window initialization
-# Your code is here
-    # The window name is root
+root = tk.Tk()
+root.title("Simple Calculator")
 
-# Display block initialization
-# Your code is here
-    # The display Entry widget's name is display
+# Entry
+display = tk.Entry(root, width = 50)
+display.grid(column = 0, row = 0, columnspan=4)
 
-
-# Buttons initialization
-# Your code is here
-    # The button list's name is buttons
+# Button
+buttons = [
+    ("7", 1, 0), ("8", 1, 1), ("9", 1, 2), ("/", 1, 3),
+    ("4", 2, 0), ("5", 2, 1), ("6", 2, 2), ("+", 2, 3),
+    ("1", 3, 0), ("2", 3, 1), ("3", 3, 2), ("-", 3, 3),
+    ("0", 4, 0), ("Clear", 4, 1), ("=", 4, 2), ("+", 4, 3),
+]
 
 for (text, row, col) in buttons:
     if text == "Clear":
